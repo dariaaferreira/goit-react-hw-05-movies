a7eae5ad7056a783c534245fc1549602cf15eea7
Daria Tkachenko <113775461+dariaaferreira@users.noreply.github.com>
2023-05-27 19:48:29 +0300

    fixed loader

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import React, { lazy, Suspense } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Loader from "components/Loader/Loader";
 import Header from '../Header/Header';
+import { LoaderContainer } from './App.styled';
 
 const Home = lazy(() => import("pages/Home/Home"));
 const Movies = lazy(() => import("pages/Movies/Movies"));
@@ -12,17 +13,17 @@ const Reviews = lazy(() => import("../Reviews/Reviews"));
 const App = () => {
   return (
     <>
-      <Suspense fallback={<Loader />}>
-        <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/movies" element={<Movies />} />
-            <Route path="/movies/:movieId" element={<MovieDetails />}>
-              <Route path="cast" element={<Cast />} />
-              <Route path="reviews" element={<Reviews />} />
-            </Route>
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
+      <Header />
+      <Suspense fallback={<LoaderContainer><Loader /></LoaderContainer>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
       </Suspense>
     </>
   );
